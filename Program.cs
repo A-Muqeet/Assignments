f7814496e3b5b8628d1aa07c2e01c618d9ae1f08
Hafiz Abdul Muqeet <hamuqeet28@gmail.com>
2024-07-14 22:46:32 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,136 +1,156 @@
-﻿using System;
-
-namespace MyApp
+﻿
+                            //Class of Book
+class Book
 {
-    internal class Program
+   public string TitleofBook;
+   public string AuthorofBook;
+   public int BookID;
+
+    public Book(string Booktitle, string Bookauthor, int bookID)
+    {
+        TitleofBook = Booktitle;
+        AuthorofBook = Bookauthor;
+        BookID = bookID;
+    }
+       public override string ToString()
+    {
+        return $"Title: {TitleofBook}, Author: {AuthorofBook}, BookID: {BookID}";
+    }
+
+                             // Class of Person
+    class Person
+    {
+       public string PersonName;
+       public  int PersonAge;
+       public  int PersonID;
+
+        public Person(string name, int age, int ID)
+        {
+            PersonName = name;
+            PersonAge = age;
+            PersonID = ID;
+        }
+
+        public override string ToString()
+        {
+            return $"Person: {PersonName}, Age: {PersonAge}, ID: {PersonID}";
+
+        }
+    }
+
+                            // Class of Librarian
+    class Librarian : Person
+    {
+        int EmployeeID;
+        public Librarian(string name, int age, int ID, int employee_id): base(name,age, ID) 
+        {
+            EmployeeID = employee_id;
+        }
+
+        public void bookissue(Book book,Person person)
+        {
+            Console.WriteLine($"{book.TitleofBook} issued to {person.PersonName}");
+            List<Book> issuebooks = new List<Book>();
+            foreach(Book i in issuebooks)
+            {
+                issuebooks.Add(i);
+            }
+        }
+        public void bookreturn(Book book, Person person)
+        {
+            Console.WriteLine($"{book.TitleofBook} returned by {person.PersonName}");
+        }
+
+    }
+                            // Class of library
+    class Library
+    {
+       public string LibraryName;
+        public int LibraryID;
+        List<Book> BookList = new List<Book>();
+        Librarian librarian = new Librarian("Wahab", 30, 05, 220);
+        public Library(string name_library, int ID)
+        {
+            LibraryName = name_library;
+            LibraryID = ID;
+           
+        }
+        public void addbook(Book book)
+        {
+            BookList.Add(book);
+        }
+        public void removebook(int ID)
+        {
+            Book removingbook = BookList.Find(book => ID == book.BookID);
+            BookList.Remove(removingbook);
+        }
+        public void viewbook()
+        {
+            foreach (Book book in BookList)
+            {
+                Console.WriteLine(book.ToString());
+            }
+        }
+        public void searchbook(string title)
+        {
+            List<Book> searchfind = BookList.FindAll(book => book.TitleofBook.Contains(title));
+            foreach (Book book in searchfind)
+            {
+                Console.WriteLine(book);
+            }
+        }
+
+    }
+
+    class Program
     {
         static void Main()
         {
-            // static void duplicate ();
-            {
-                int[] numbers = { 1, 2, 2, 3, 4, 4 };
-                List<int> numbersList = new List<int>(numbers); //Converting to list
-                for (int i = 1; i < numbers.Length; i++)
-                {
-                    if (numbersList[i] == numbersList[i - 1]) ; //Equality with preceeding number
-                    numbersList.RemoveAt(i);
-                    Console.WriteLine(numbersList[i - 1] + "");
-                }
-            }
-            // static void largest ();
-            {
-                int[] numbers_1 = { 1, 5, 3, 4, 2 };
-                List<int> numbersList_1 = new List<int>(numbers_1);
-                int a = numbersList_1.Max();                        //Obtaining first maximum value
-                for (int i = 0; i < numbersList_1.Count; i++)
-                {
-                    numbersList_1.Remove(a);                        //Removing first maximum value
-                    numbersList_1.Max();
-                }
-                Console.WriteLine("" + a);
-                Console.WriteLine("" + numbersList_1.Max());
-            }
-            // static void lastzeros ();
-            {
-                int[] numbers_2 = { 1, 5, 3, 4, 2 };
-                List<int> numbersList_2 = new List<int>(numbers_2);
-                int a = numbers_2.Length;                           //Total length of array
-                int b = a - 1;                                        // Number of indexes
-                for (int i = 0; i < a; i++)
-                {
-                    int c = numbers_2[b] = 0;
-                    Console.WriteLine("" + numbers_2[i]);
-                }
-            }
-            // static void MergeArrays ();
-            {
-                int[] firstarray = { 1, 3, 5 };
-                int[] secondarray = { 2, 4, 6 };
 
-                for (int z = 0; z < firstarray.Length; z++)
-                {
-                    for (int j = 0; j < secondarray.Length; j++)
-                    {
-                        if (firstarray[z] < secondarray[j]) ;   
-                        int[] finalarray = { firstarray[z], secondarray[j] };   //Stacking array elements
-                        for (int i = 0; i < firstarray.Length; i++)
-                        {
-                            Console.WriteLine("" + finalarray[i]);
-                        }
-                    }
+            //Creating instance of Library
+            Library library1 = new Library("National Library", 01);
 
-                }
-            }
-            // static void MissingNumber ();
-            {
-                int[] samplearray = { 1, 3, 5, 4 };
-                List<int> samplelist = new List<int>(samplearray);
-                for (int i = 0; i < samplearray.Length; i++)
-                {
-                    int[] comparray = { 1, 2, 3, 4 };
-                    if (samplearray[i] != comparray[i])
-                        Console.WriteLine("" + samplearray[i]);
+            // Creating instance of Librarian
+            Librarian librarian1 = new Librarian("Ali", 25, 01, 210);
 
-                }
-            }
-            // static void ArmstrongNumber ();
-            {
-                Console.Write("Enter number");
-                int A = int.Parse(Console.ReadLine());
-                while (A >= 1)
-                {
-                    int a = A % 10;                             //1st place value
-                    int c = 0;
-                    c = c + (a ^ 3);
-                    A = A / 10;                                  //2nd place value
-                    if (c == A)
-                    {
-                        Console.WriteLine("Armstrong Number");
-                    }
-                    else if (c != A)
-                    {
-                        Console.WriteLine("Not Armstrong Number");
+            // Creating instances for user
+            Person person1 = new Person("Ahmad", 30, 1);
+            Person person2 = new Person("Hasan", 28, 2);
+            Person person3 = new Person("Murtaza", 26, 3);
 
-                    }
-                }
-            }
-            //static void LargestPrefix()
-            {
-                string[] samplearray1 = new string[] { "Punish", "Punt" };
-                string a = samplearray1[0];
-                string b = samplearray1[1];
-                string largeprefix = "";
-                int c = samplearray1[0].Length;
-                int d = samplearray1[1].Length;
-                for (int i = 0; i < c; i++)
-                    for (int j = 0; j < d; j++)
-                    {
-                        if (a[i] == b[j])
-                            break;
-                        largeprefix += a[i];
-                        Console.WriteLine("" + largeprefix);
-                    }
-            }
-            //static void Fibonacci()
-            {
-                Console.Write("Enter number greater than 1 ");
-                int n = int.Parse(Console.ReadLine());
-                int[] newarray = new int[n + 1];
-                newarray[0] = 0; newarray[1] = 1;           //Specifying 0 and 1 index value
-                for (int i = 2; i < n; i++)
-                {
-                    newarray[i] = newarray[i - 2] + newarray[i - 1];
-                   Console.WriteLine("" + newarray[i]);
-                }
-            }
+
+            // Creating instances of Book
+            Book first_book = new Book("Fluid Mechanics", "Frank White", 1);
+            Book second_book = new Book("Thermodynamics", "Moran Shapiro", 2);
+            Book third_book = new Book("Statics", "Hibbler", 3);
+            Book fourth_book = new Book("Heat Transfer", "JP Morgan", 4);
+
+            //Issuing book to user
+            librarian1.bookissue(first_book, person2);
+            librarian1.bookissue(fourth_book, person1);
+
+            // Return of book by user
+            librarian1.bookreturn(fourth_book, person1);
+
+            // Adding Book to library
+
+            library1.addbook(first_book);
+            library1.addbook(second_book);
+            library1.addbook(third_book);
+            library1.addbook(fourth_book);
+            library1.viewbook();
+
+            // Removing book from library
+
+            library1.removebook(2);
+
+            // Viewing Final list after removal of book
+            library1.viewbook();
+
+            // Searching book
+            library1.searchbook("Statics");
+
+
         }
     }
 }
-
-
-
-
-
-
-
-
